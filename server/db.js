b5ecef2adac71254a997a2f3c110fecc0f18f924
thy <972143891@qq.com>
2020-03-21 22:07:48 +0800

    chore: add depen

---
+++ server/db.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+
+const client = (sql, arg, callback) => {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "123456",
+    database: "purplevine",
+  });
+
+  connection.connect();
+
+  connection.query(sql, arg, function(error, results, fields) {
+    if (error) throw error;
+    callback && callback(results, fields);
+  });
+
+  connection.end();
+};
+
+module.exports = client;
---
+++ server/router.js
@@ -0,0 +1,95 @@
+var formidable = require('formidable');
+var util = require('util');
+var db = require('./db');
+// import client from './db';
+
+module.exports = {
+  index: function(req, res) {
+    res.setHeader("Content-Type", "text/html;charset=utf-8");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    // res.setHeader("Access-Control-Allow-Headers","Content-Type,Content-Length,Authorization,Accept,X-Requested-With");
+    // res.setHeader("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+
+
+    var sql = 'select * from note_list';
+    db(sql,[], (data) => {
+      console.log('data',data);
+      res.writeHead(200, "ok");
+      res.write("Hello Node Server");
+      res.end();
+    })
+  },
+  simpleCrossOriginRequest: function(req, res) {
+    var data = JSON.stringify({
+      code: 200,
+      data: {
+        simpleCrossOrigin: true
+      }
+    });
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type,Content-Length,Authorization,Accept,X-Requested-With"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "PUT,POST,GET,DELETE,OPTIONS"
+    );
+    res.writeHead(200, "ok");
+    res.end(data);
+  },
+  notSimpleCrossOriginRequest: function(req, res) {
+    var data = JSON.stringify({
+      code: 200,
+      data: {
+        notSimpleCrossOrigin: true
+      }
+    });
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type,Content-Length,Authorization,Accept,X-Requested-With"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "PUT,POST,GET,DELETE,OPTIONS"
+    );
+    res.writeHead(200, "ok");
+    res.end(data);
+  },
+  jsonp: function(req, res, queryObj) {
+    var data = JSON.stringify({ isJSONP: true });
+    var callBack = queryObj.callBack;
+    // console.log(callBack);
+    res.setHeader("Content-Type", "application/json;charset=utf-8");
+    res.writeHead(200, "ok");
+    var backData = callBack + "(" + data + ")";
+    res.end(backData);
+  },
+  upload: function(req, res, queryObj) {
+
+
+    var form = new formidable.IncomingForm();
+
+    form.parse(req, function(err, fields, files) {
+      var data = JSON.stringify({
+        code: 200,
+        data: {fields: fields, files: files}
+      });
+
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type,Content-Length,Authorization,Accept,X-Requested-With"
+      );
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "PUT,POST,GET,DELETE,OPTIONS"
+      );
+      res.writeHead(200,'ok');
+      res.end(data);
+    });
+
+    return;
+  }
+};
