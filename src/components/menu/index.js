8802770fd066ecbed3c7a60fc077549404328f4e
thy <972143891@qq.com>
2020-04-13 12:21:26 +0800

    feat: add table component

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,28 +1,23 @@
 import React from 'react';
 import { Menu as AntMenu } from 'antd';
 import {
-  UserOutlined,
-  UploadOutlined,
-  VideoCameraOutlined,
+  ProjectOutlined,
+  ContainerOutlined,
 } from '@ant-design/icons';
 import styles from './index.module.css';
 
 function Menu() {
   return (
     <>
-      <div className={styles.logo} />
-      <AntMenu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+      <div className={styles.logo}>User Tom</div>
+      <AntMenu theme="dark" mode="inline" defaultSelectedKeys={['2']}>
         <AntMenu.Item key="1">
-          <UserOutlined />
-          <span className="nav-text">nav 1</span>
+          <ProjectOutlined />
+          <span className="nav-text">Projects</span>
         </AntMenu.Item>
         <AntMenu.Item key="2">
-          <VideoCameraOutlined />
-          <span className="nav-text">nav 2</span>
-        </AntMenu.Item>
-        <AntMenu.Item key="3">
-          <UploadOutlined />
-          <span className="nav-text">nav 3</span>
+          <ContainerOutlined />
+          <span className="nav-text">Issues</span>
         </AntMenu.Item>
       </AntMenu>
     </>
---
+++ src/components/table/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Table as AntTable } from 'antd';
+import styles from './index.module.css';
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+  },
+];
+
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: text => <a>{text}</a>,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (text, record) => (
+      <span>
+        <a style={{ marginRight: 16 }}>Invite {record.name}</a>
+        <a>Delete</a>
+      </span>
+    ),
+  },
+];
+
+function Table() {
+  return (
+    <>
+      <AntTable columns={columns} dataSource={data} />
+    </>
+  );
+}
+
+export default Table;
