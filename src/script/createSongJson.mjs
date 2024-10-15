a8d7f442a968adddc532cd35415f4dc47bd1557b
thy <972143891@qq.com>
2024-10-15 16:53:17 +0800

    feat: add 5459

--- src/script/createSongJson.mjs
+++ src/script/createSongJson.mjs
@@ -4,9 +4,9 @@ import { fileURLToPath } from "node:url";
 import { createSingerAllSong } from './generateSingerSong.mjs';
 import { formatSingersData } from './formatSinger.mjs';
 
-const targetSingerId = 53;
-const targetSingerName = '潘玮柏';
-const startNum = 410;
+const targetSingerId = 59;
+const targetSingerName = '张震岳';
+const startNum = 440;
 
 // 这个是针对 pm2 启动时无法找到路径的问题
 const fileName = fileURLToPath(import.meta.url);
