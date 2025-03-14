17f119c73718db76e55c7a5e054ff9e66bddfa6d
XXHolic <972143891@qq.com>
2025-03-14 21:48:25 +0800

    feat: add 47 new singer

--- src/script/createSongJson.mjs
+++ src/script/createSongJson.mjs
@@ -4,9 +4,9 @@ import { fileURLToPath } from "node:url";
 import { createSingerAllSong } from './generateSingerSong.mjs';
 import { formatSingersData } from './formatSinger.mjs';
 
-const targetSingerId = 168;
-const targetSingerName = '周思涵';
-const startNum = 747;
+const targetSingerId = 215;
+const targetSingerName = "Avicii";
+const startNum = 820;
 const tags = []; // 纯音乐时，加入初始值 1，其它默认空数组
 
 // 这个是针对 pm2 启动时无法找到路径的问题
